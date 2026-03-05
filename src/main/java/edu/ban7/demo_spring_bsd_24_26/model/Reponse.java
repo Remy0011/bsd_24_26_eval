0ff1382c6111f52eb0b917de4e700f57928b8af9
Remy0011 <remy.deiber1999@gmail.com>
2026-03-05 14:30:00 +0100

    add getter setter and entity

--- src/main/java/edu/ban7/demo_spring_bsd_24_26/model/Reponse.java
+++ src/main/java/edu/ban7/demo_spring_bsd_24_26/model/Reponse.java
@@ -1,9 +1,15 @@
 package edu.ban7.demo_spring_bsd_24_26.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Reponse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
